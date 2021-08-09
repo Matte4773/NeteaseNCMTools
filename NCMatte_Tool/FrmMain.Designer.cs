957610e55883649d59a0611417078ff4e37a7822
RatherCheck <80532489+Matte4773@users.noreply.github.com>
2021-08-09 21:49:36 +0800

    XSM

---
+++ NCMatte_Tool/FrmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace NCMatte_Tool
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.uiTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // uiTimer
+            // 
+            this.uiTimer.Interval = 1;
+            this.uiTimer.Tick += new System.EventHandler(this.uiTimer_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.CaptionColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(512, 352);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMain";
+            this.Padding = new System.Windows.Forms.Padding(18);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NCM转换工具 By Matte4773 Ver:1.4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer uiTimer;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# NCM Tools-gui 2021.8.9 21047
+
+Netease NCM File Converter 
