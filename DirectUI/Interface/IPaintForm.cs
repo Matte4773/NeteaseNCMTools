47d08926e039e9287ba2d3b11943b40156656157
RatherCheck <80532489+Matte4773@users.noreply.github.com>
2022-07-31 19:34:24 +0800

    Add files via upload

---
+++ DirectUI/Interface/IPaintForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectUI.Interface
+{
+    public interface IPaintForm : IPaintControl
+    {
+        void PaintCaption(PaintEventArgs g);
+        void PaintClose(PaintEventArgs e);
+
+        RectangleF GetCaptionBounds();
+    }
+}
