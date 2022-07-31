1c7a4fd1c043574dd4a3854099962c20b440eca3
RatherCheck <80532489+Matte4773@users.noreply.github.com>
2022-07-31 19:18:23 +0800

    package

---
+++ DirectUI/HightQualityGraphics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectUI
+{
+    public class HightQualityGraphics : IDisposable
+    {
+        private Graphics _g;
+
+        public HightQualityGraphics(Graphics g)
+        {
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            _g = g;
+        }
+
+        public void Dispose()
+        {
+            _g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+            _g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+            _g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.Default;
+        }
+    }
+}
